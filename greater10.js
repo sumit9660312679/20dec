03eeb91c7bafb41250e493382ce2fa080b6c307f
sumit9660312679 <87266670+sumit9660312679@users.noreply.github.com>
2021-12-30 18:03:14 +0530

    add initial website

---
+++ greater10.js
@@ -0,0 +1,7 @@
+var a=10;
+var b=8;
+if (a>b){
+  console.log(a)
+}else{
+  console.log(b)
+}
---
+++ greatestnumber.js
@@ -0,0 +1,13 @@
+var a=10;
+var b=8;
+var c=5;
+if (a>b){
+  console.log(a)
+}else{
+  console.log(b)
+}
+if(b>c){
+  console.log(b)
+}else{
+  console.log(c)
+}
